253ee9f1972cc00e77f8f09120274de023aa8100
irham3 <irhamtri@student.telkomuniversity.ac.id>
2021-09-28 06:57:28 +0700

    HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld  {
+    public static void main(String[] args) {
+        System.out.println("Nama: Irham Tri Ahmadi");
+        System.out.println("NIM: 6706213077");
+        System.out.println("Kelas: D3IF-45-01");
+    }
+}
